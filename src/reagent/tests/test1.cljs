05d1e67f5d29354ca6b8c46e3f43586377dde7f4
mkersh <kershaw.mark@gmail.com>
2022-01-05 07:26:50 +0000

    update

--- src/reagent/tests/test1.cljs
+++ src/reagent/tests/test1.cljs
@@ -1,19 +1,23 @@
 (ns reagent.tests.test1
   (:require [reagent.dom :as rdom]))
 
+(defn very-simple []
+  [:div "Hello World!!"])
+
 (defn simple-component [name]
-  [:div#app
+  [:div
    [:p (str "I am a component! " name)]
    [:p.someclass
     "I have " [:strong "bold"]
     [:span {:style {:color "red"}} " and red "] "text."]])
 
-(defn render-simple [name]
+(defn render [component]
   (rdom/render
-   [simple-component name]
+   component
    ;; (.-body js/document) ;; alternative to next
    (js/document.getElementById "app")))
 
 (comment
-(render-simple "Mark")
+(render very-simple)
+(render (simple-component "Mark 222"))
 )
